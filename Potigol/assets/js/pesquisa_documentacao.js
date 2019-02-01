5b7e76b168899b7019c26ff0524f907be8aedd25
Luan Henrique <luanhgs0111@gmail.com>
2019-01-31 22:30:43 -0300

    Trechos de códigos para copiar passados para um arquivo XML

--- Potigol/assets/js/pesquisa_documentacao.js
+++ Potigol/assets/js/pesquisa_documentacao.js
@@ -60,3 +60,16 @@ window.addEventListener("scroll", function(){
         }
     }
 });
+
+//ler xml externo
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
