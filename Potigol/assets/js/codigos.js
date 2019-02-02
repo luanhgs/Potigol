b3dbf8a16681c9f5d2f5fa83ecc0c923e80b8d66
Luan Henrique <luanhgs0111@gmail.com>
2019-02-01 21:14:53 -0300

    Organização do código

--- Potigol/assets/js/codigos.js
+++ Potigol/assets/js/codigos.js
@@ -1,7 +1,7 @@
 var xmlCodigos = null;
 
 //ler e converter xml
-readTextFile("../assets/codigos/cod.xml", function(text){
+readTextFile("../assets/data/codigos.xml", function(text){
     if (window.DOMParser) {
         let parser = new DOMParser();
         xmlCodigos = parser.parseFromString(text,"text/xml");
@@ -13,62 +13,31 @@ readTextFile("../assets/codigos/cod.xml", function(text){
     } 
 });
 
-////
-////
-////
+
 //copiar codigo
 const buttons = document.querySelectorAll('button[data-codigo-id]');
-buttons.forEach(function(btn){
-    btn.addEventListener("click", function(){ 
-        const codigo = getCodigoXML(this.getAttribute("data-codigo-id"));
-        console.log(codigo);
-        copyToClipboard(codigo);
-    });  
-});
-
-function getCodigoXML(id){
-    for(var i = 0; i <= xmlCodigos.querySelectorAll("codigo").length - 1; i++){
-        if(xmlCodigos.querySelectorAll("codigo")[i].querySelector("id").childNodes[0].nodeValue === id)
-            return xmlCodigos.querySelectorAll("codigo")[i].querySelector("txt").childNodes[0].nodeValue;
-    }
-}
-
-// Copies a string to the clipboard. Must be called from within an event handler such as click.
-// May return false if it failed, but this is not always
-// possible. Browser support for Chrome 43+, Firefox 42+, Edge and IE 10+.
-// No Safari support, as of (Nov. 2015). Returns false.
-// IE: The clipboard feature may be disabled by an adminstrator. By default a prompt is
-// shown the first time the clipboard is used (per session).
-function copyToClipboard(text) {
-    if (window.clipboardData && window.clipboardData.setData) {
-        // IE specific code path to prevent textarea being shown while dialog is visible.
-        return clipboardData.setData("Text", text); 
-
-    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
-        textarea.textContent = text;
-        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
-        document.body.appendChild(textarea);
-        textarea.select();
-        try {
-            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
-        } catch (ex) {
-            console.warn("Copy to clipboard failed.", ex);
-            return false;
-        } finally {
-            document.body.removeChild(textarea);
-        }
-    }
-}
-
-////
-////
-//botão animação
 buttons.forEach(function(btn, i){
-    btn.addEventListener("click", function(){
-        document.body.querySelectorAll(".content-documentation .section-documentation .demonstration-documentation")[i].classList.add("copied-code");
-        setTimeout(function(){
-            document.body.querySelectorAll(".content-documentation .section-documentation .demonstration-documentation")[i].classList.remove("copied-code");
-        }, 400);
+    btn.addEventListener("click", function(){ 
+        const codigo = getCodigo(this.getAttribute("data-codigo-id"));
+        if(codigo != null){
+            //copiar para clipboard
+            copyToClipboard(codigo);
+
+            //animar botão
+            document.body.querySelectorAll(".content-documentation .section-documentation .demonstration-documentation")[i].classList.add("copied-code");
+            setTimeout(function(){
+                document.body.querySelectorAll(".content-documentation .section-documentation .demonstration-documentation")[i].classList.remove("copied-code");
+            }, 400);
+        }
     });  
 });
+
+function getCodigo(id){
+    for(var i = 0; i <= xmlCodigos.querySelectorAll("codigo").length - 1; i++){
+        let codigo = xmlCodigos.querySelectorAll("codigo")[i];
+
+        if(codigo.querySelector("id").childNodes[0].nodeValue === id)
+            return codigo.querySelector("txt").childNodes[0].nodeValue;
+    }
+    return null;
+}
--- Potigol/assets/js/projetos.js
+++ Potigol/assets/js/projetos.js
@@ -1,27 +1,16 @@
-//array de objetos contendo as informações de cada projeto
-var projetos = [
-    {
-        codigo: "CAOS-GAME",
-        nome: "Caos Game - Rogue Like 2D",
-        descricao: "Jerimum-Rogue e um jogo inspirado no estilo rogue-like onde seu personagem tem que sobreviver ao ataque de uma orda de inimigos. Inicialmente está sendo desenvolvido para um trabalho da disciplina de programação de computadores da turma IFRN.2017.1.M orientado pelo professor Leonardo Lucena.",
-        imgUrl: "../assets/img/projetos/caos-game.png",
-        githubUrl: "https://saulodfp.github.io/jerimum-rogue/"
-    },
-    {
-        codigo: "POTIGAME",
-        nome: "PotiGame",
-        descricao: "Neste jogo, o jogador entra na pele de um homem com tanta fome q ficou amarelo, e está desesperado a procura de comida em um labirinto maluco. Seu objetivo final é encontrar a rosquinha antes que desmanhe de fome.",
-        imgUrl: "../assets/img/projetos/potigame.png",
-        githubUrl: "https://tiagolisboa.github.io/PotiGame/"
-    },
-    {
-        codigo: "BOBOT-VS-ZOMBIES",
-        nome: "Robot Vs Zombies",
-        descricao: "É um jogo 2d feito com a linguagem Potigol. No jogo, você controla um robô, que é o personagem principal, e tem como objetivo matar os zumbis que vão aparecendo.",
-        imgUrl: "../assets/img/projetos/robot-vs-zombies.png",
-        githubUrl: "https://pjeferson.github.io/RobotVsZombies/"
+var xmlProjetos = null;
+
+readTextFile("../assets/data/projetos.xml", function(text){
+    if (window.DOMParser) {
+        let parser = new DOMParser();
+        xmlProjetos = parser.parseFromString(text,"text/xml");
     }
-];
+    else {
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = false;
+        xmlProjetos.loadXML(text);
+    } 
+});
 
 //fechar janela de projeto
 document.querySelector("#view-projeto .projeto .btn-close-projeto").addEventListener("click", function(){
@@ -38,30 +27,38 @@ window.addEventListener("keyup", function(){
     
 });
 
+//abrir informações do projeto
 const btns_sproj = document.querySelectorAll(".projetos .box-projeto[data-codigo-projeto]");
-
 btns_sproj.forEach(function(el){
     el.addEventListener("click", function(){
         const codigo_proj = this.getAttribute("data-codigo-projeto");
         const projeto = getProjeto(codigo_proj);
-        
-        //---adicionar informações ---
-        //nome
-        document.querySelector("#view-projeto .projeto .img-title-projeto .title").innerHTML = projeto.nome;
-        //imagem
-        document.querySelector("#view-projeto .projeto .img-title-projeto img").src = projeto.imgUrl;
-        //descrição
-        document.querySelector("#view-projeto .projeto .desc span").innerHTML = projeto.descricao;
-        //link github
-        document.querySelector("#view-projeto .projeto .opt .btn-github").href = projeto.githubUrl;   
 
-        //
-        document.body.classList.add("projeto-open");
+        if(projeto != null){
+            //nome
+            document.querySelector("#view-projeto .projeto .img-title-projeto .title").innerHTML = 
+                projeto.querySelector("nome").childNodes[0].nodeValue;
+            //imagem
+            document.querySelector("#view-projeto .projeto .img-title-projeto img").src = 
+                projeto.querySelector("imgUrl").childNodes[0].nodeValue;
+            //descrição
+            document.querySelector("#view-projeto .projeto .desc span").innerHTML = 
+                projeto.querySelector("descricao").childNodes[0].nodeValue;
+            //link github
+            document.querySelector("#view-projeto .projeto .opt .btn-github").href = 
+                projeto.querySelector("githubUrl").childNodes[0].nodeValue;
+
+            document.body.classList.add("projeto-open");
+        }
     });
 });
 
 function getProjeto(codigo_proj){
-    for(var i = 0; i <= projetos.length; i++)
-        if(codigo_proj == projetos[i].codigo)
-            return projetos[i];  
+    for(var i = 0; i <= xmlProjetos.querySelectorAll("projeto").length - 1; i++){
+        let projeto = xmlProjetos.querySelectorAll("projeto")[i];
+
+        if(projeto.querySelector("codigo").childNodes[0].nodeValue == codigo_proj)
+            return projeto;
+    }
+    return null;
 }
