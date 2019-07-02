e4dbf6d712d2c97b4903e66d89c0260f16552d8b
Luan Henrique <luanhgs0111@gmail.com>
2019-07-02 15:58:13 -0300

    Revisão do código

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -1,7 +1,6 @@
 //ler arquivo externo
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
-    //rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
     rawFile.onreadystatechange = function() {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
